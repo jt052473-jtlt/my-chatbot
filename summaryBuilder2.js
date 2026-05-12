7513d35e6febc0ab9de5cefb55f8a0b69bbd6db0
jt052473-jtlt <jt052473@gmail.com>
2026-05-12 03:50:08 -0400

    Update summaryBuilder2.js

--- summaryBuilder2.js
+++ summaryBuilder2.js
@@ -2,11 +2,20 @@ function buildSummary(answers) {
     const labels = translations[currentLanguage].summaryLabels;
 
     return `
-${labels.name}: ${answers.name || ""}
-${labels.dob}: ${answers.dob || ""}
-${labels.reason}: ${answers.reason || ""}
-${labels.duration}: ${answers.duration || ""}
-${labels.allergies}: ${answers.allergies || ""}
+${labels.name}:
+${answers.name || ""}
+
+${labels.dob}:
+${answers.dob || ""}
+
+${labels.reason}:
+${answers.reason || ""}
+
+${labels.duration}:
+${answers.duration || ""}
+
+${labels.allergies}:
+${answers.allergies || ""}
     `.trim();
 }
 
